3a102e1488be7ed088a637f7b10e7d9db819e88d
daniel-nicolai-hansten <74103470+daniel-nicolai-hansten@users.noreply.github.com>
2023-02-27 16:28:28 +0100

    .

--- src/serial.rs
+++ src/serial.rs
@@ -28,6 +28,14 @@ fn translate_io_errors(err: std::io::Error) -> nb::Error<SerialError> {
         err => nb::Error::Other(SerialError { err }),
     }
 }
+fn translate_io_errors_b(err: std::io::Error) -> Error<SerialError> {
+    match err.kind() {
+        IoErrorKind::WouldBlock | IoErrorKind::TimedOut | IoErrorKind::Interrupted => {
+            nb::Error::WouldBlock
+        }
+        err => nb::Error::Other(SerialError { err }),
+    }
+}
 
 impl embedded_hal::serial::ErrorType for Serial {
     type Error = SerialError;
@@ -58,12 +66,12 @@ impl embedded_hal_nb::serial::Write<u8> for Serial {
 
 impl embedded_hal::serial::Write<u8> for Serial {
     fn write(&mut self, word: &[u8]) -> Result<(), Self::Error> {
-        self.0.write(word)?;
+        self.0.write(word).map_err(translate_io_errors_b)?;
         Ok(())
     }
 
     fn flush(&mut self) -> Result<(), Self::Error> {
-        self.0.flush()
+        self.0.flush().map_err(translate_io_errors_b)
     }
 }
 
